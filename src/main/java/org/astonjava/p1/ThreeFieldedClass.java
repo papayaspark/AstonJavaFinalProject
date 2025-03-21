2fa9a257f32916b3a16466941e4f8153adac8b32
Khazhmurat Kairov <papayaspark@protonmail.com>
2025-03-21 16:44:48 +0300

    Add interface for the three classes

---
+++ src/main/java/org/astonjava/p1/ThreeFieldedClass.java
@@ -0,0 +1,9 @@
+package org.astonjava.p1;
+
+// TODO: Общий интерфейс для трех классов. Может измениться по ходу проекта.
+
+public interface ThreeFieldedClass {
+
+    /* Проверяет, валидная ли строка или нет. */
+    boolean validateString(String inputString);
+}
