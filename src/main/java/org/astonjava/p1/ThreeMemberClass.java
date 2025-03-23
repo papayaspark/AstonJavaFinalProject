278ba1f0e6d1134a5e16b690a262c64c226a0151
Khazhmurat Kairov <papayaspark@protonmail.com>
2025-03-23 14:14:46 +0300

    Remove interface ThreeFieldedClass. Add abstract ThreeMemberClass

---
+++ src/main/java/org/astonjava/p1/ThreeMemberClass.java
@@ -0,0 +1,24 @@
+package org.astonjava.p1;
+
+// Общий абстрактный класс для трех классов. Может измениться по ходу проекта.
+
+import java.util.regex.Pattern;
+
+public abstract class ThreeMemberClass {
+
+  static Pattern validationPattern;
+  static String member1RuName = "Поле 1";
+  static String member2RuName = "Поле 2";
+  static String member3RuName = "Поле 3";
+
+  /* Проверяет, валидная ли строка или нет. */
+  public static boolean validateString(String inputString) {
+    return validationPattern.matcher(inputString).matches();
+  }
+
+  public abstract String toString();
+
+  public abstract boolean equals(Object o);
+
+  public abstract int hashCode();
+}
