ac4c9af66f880705946e737a107b68f13581f24d
Khazhmurat Kairov <papayaspark@protonmail.com>
2025-03-24 13:19:28 +0300

    Add getValidOption() method and refactor main() methon

--- src/main/java/org/astonjava/p1/Main.java
+++ src/main/java/org/astonjava/p1/Main.java
@@ -2,54 +2,98 @@ package org.astonjava.p1;
 
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.Set;
 
 public class Main {
+
+  static Scanner scanner = new Scanner(System.in);
+  static boolean failOnInvalid = true;
+
   public static void main(String[] args) {
 
     System.out.println("AstonJavaFinalProject 1.0 by Gang of Five, Inc.\n");
 
     boolean dataLoaded = false;
-    ArrayList<ThreeMemberClass> data;
+    ArrayList<ThreeMemberClass> data = null;
 
-    Scanner scanner = new Scanner(System.in);
     String userInput = "";
 
     while (!userInput.equalsIgnoreCase("Q")) {
 
-      System.out.println(
-          "Выберите действие:\n"
-              + "1. Загрузка данных;\n"
-              + "2. Выбор способа сортировки;\n"
-              + "3. Сортировка данных;\n"
-              + "4. Поиск по данным;\n"
-              + "5. Запись данные в файл;\n"
-              + "Q. Выход.\n");
+      userInput = getValidInputOption(
+              "Выберите действие:\n"
+                  + "1. Загрузка данных;\n"
+                  + "2. Выбор способа сортировки;\n"
+                  + "3. Сортировка данных;\n"
+                  + "4. Поиск по данным;\n"
+                  + "5. Запись данные в файл;\n"
+                  + "Q. Выход.\n",
+              Set.of("1", "2", "3", "4", "5", "Q"));
 
-      userInput = scanner.nextLine();
-
+      System.out.println("Input: " + userInput);
+      //
+      //
       switch (userInput) {
         case "1":
-          dataLoaded = true;
+          //
+          System.out.println("data=" + data);
+//          data = loadData();
           break;
-        case "2":
-          System.out.println("Выбор способа сортировки");
-          break;
-        case "3":
-          System.out.println("Отсортированные данные");
-          break;
-        case "4":
-          System.out.println("Поиск по данным");
-          break;
-        case "5":
-          System.out.println("Запись в файл");
-          break;
+        //        case "2":
+        //          System.out.println("Выбор способа сортировки");
+        //          break;
+        //        case "3":
+        //          System.out.println("Отсортированные данные");
+        //          break;
+        //        case "4":
+        //          System.out.println("Поиск по данным");
+        //          break;
+        //        case "5":
+        //          System.out.println("Запись в файл");
+        //          break;
         case "q":
         case "Q":
           System.out.println("Выход");
           break;
-        default:
-          System.out.println("Выбранного варианта нет в списке. Введите корректное значение");
       }
     }
+  }
+
+//  static ArrayList<ThreeMemberClass> loadData() {
+//    Scanner scanner = new Scanner(System.in);
+//    String userInput = "";
+//    while (!userInput.equalsIgnoreCase("Q")) {
+//      userInput = getValidInputOption(
+//          "Выберите вариант загрузки данных:\n"
+//              + "1. В интерактивном режиме\n"
+//              + "2. Из файла\n"
+//              + "3. Заполнение случайными данными\n"
+//              + "Q. Выход\n", Set.of("1", "2", "3"));
+//      userInput = scanner.nextLine();
+//
+//      switch (userInput) {
+//        case "1":
+//          break;
+//        case "2":
+//          break;
+//        case "3":
+//          break;
+//        case "Q":
+//          return null;
+//      }
+//    }
+//
+//    return null;
+//  }
+
+  // Выводит приветствие и ждет выбора корректного значения
+  static String getValidInputOption(String greeting, Set<String> validOptions) {
+    System.out.println(greeting);
+    String inputString = scanner.nextLine();
+    while (!validOptions.contains(inputString)) {
+      System.out.println("Выбранного варианта нет в списке. Введите корректное значение");
+      inputString = scanner.nextLine();
+    }
+    return inputString;
   }
 }
