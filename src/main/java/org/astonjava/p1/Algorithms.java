db6a264dc1ae256d75d4099c26c3ecb86d5881c7
Khazhmurat Kairov <papayaspark@protonmail.com>
2025-03-25 00:55:04 +0300

    Add Algorithms

---
+++ src/main/java/org/astonjava/p1/Algorithms.java
@@ -0,0 +1,14 @@
+package org.astonjava.p1;
+
+import java.util.Comparator;
+
+public class Algorithms {
+
+    // TODO: Вынести сюда быструю сортировку
+    // Пока можно считать CustomList обычным LinkedList
+    public static void quickSort(CustomList<ThreeMemberClass> list) {}
+
+    public static Integer binarySearch(CustomList<ThreeMemberClass> list) {
+        return 0;
+    }
+}
