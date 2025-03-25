75a9815350eceea4a11df28eff04be78a40151c0
Khazhmurat Kairov <papayaspark@protonmail.com>
2025-03-25 14:22:11 +0300

    Add RandomDataLoader

---
+++ src/main/java/org/astonjava/p1/RandomDataLoader.java
@@ -0,0 +1,45 @@
+package org.astonjava.p1;
+
+import java.lang.reflect.Constructor;
+import java.util.Scanner;
+
+// Возвращает заполненный случайными данными CustomList
+public class RandomDataLoader implements DataLoaderInterface {
+  @Override
+  public CustomList<ThreeMemberClass> loadData(Class<? extends ThreeMemberClass> tmclass) {
+
+    try {
+      Scanner scanner = new Scanner(System.in);
+      System.out.print("Введите нужное количество записей: ");
+      int length = scanner.nextInt();
+
+      CustomList<ThreeMemberClass> result = new CustomList<>();
+
+      Class<?>[] constructorArgs = new Class[] {};
+      Constructor<? extends ThreeMemberClass> tmConstructor =
+          tmclass.getDeclaredConstructor(constructorArgs);
+
+      for (int i = 0; i < length; i++) result.add(tmConstructor.newInstance());
+
+      return result;
+
+    } catch (Exception e) {
+      return null;
+    }
+  }
+
+  // TODO: Убрать после тестов
+  public static void main(String[] args) {
+    RandomDataLoader randomDataLoader = new RandomDataLoader();
+
+    CustomList<ThreeMemberClass> result = randomDataLoader.loadData(Bus.class);
+
+    if (result != null) {
+      for (var d : result) {
+        System.out.println(d);
+      }
+    } else {
+      System.out.println(result);
+    }
+  }
+}
