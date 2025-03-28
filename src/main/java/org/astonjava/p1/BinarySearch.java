d955a681c82c6630c1a8fee27b4517b0e1a34189
Khazhmurat Kairov <papayaspark@protonmail.com>
2025-03-28 11:30:18 +0300

    BinarySearch works on ints

--- src/main/java/org/astonjava/p1/BinarySearch.java
+++ src/main/java/org/astonjava/p1/BinarySearch.java
@@ -1,24 +1,25 @@
 package org.astonjava.p1;
 
+import java.util.Arrays;
 import java.util.Comparator;
 
 public class BinarySearch {
   public static <T> int search(
       T[] sortedArray, T key, int low, int high, Comparator<T> comparator) {
-    int index = Integer.MAX_VALUE;
 
+//    System.out.println("Got low=" + low + "\nGot high=" + high);
     while (low <= high) {
-      int mid = low + ((high - low) / 2);
+//      System.out.printf("%s %s\n", low, high);
+      int mid = (high + low) / 2;
       if (comparator.compare(sortedArray[mid], key) < 0) {
         low = mid + 1;
       } else if (comparator.compare(sortedArray[mid], key) > 0) {
         high = mid - 1;
       } else if (comparator.compare(sortedArray[mid], key) == 0) {
-        index = mid;
-        break;
+        return mid;
       }
     }
-    return index;
+    return -1;
   }
 
   public static void main(String[] args) {
@@ -27,11 +28,20 @@ public class BinarySearch {
 //    QuickSort.quickSort(array, 0, array.length - 1, Comparator.naturalOrder());
     Integer[] array2 = new Integer[] {48, 76, 30, 50, 34, 48, 69, 72, 59, 47};
     QuickSort.quickSort(array2, 0, array2.length - 1, Comparator.naturalOrder());
+    System.out.println(Arrays.toString(array2));
+    System.out.println(array2.length);
 
 
+//    for (int i = 0; i < 10; i++) System.out.printf("%s %s\n", array[i], array2[i]);
 
-    for (int i = 0; i < 10; i++) System.out.printf("%s %s\n", array[i], array2[i]);
+    for (int i = 0; i < 10; i++)
+      System.out.printf(
+          "%s -> %s\n",
+          array[i], search(array2 , array[i], 0 ,array2.length-1, Integer::compareTo));
 
-    for (int i = 0; i < 10; i++) System.out.printf("%s -> %s\n", array[i], search(array2, array[i],0, args.length - 1, Comparator.naturalOrder()));
+
+//    System.out.println(
+//            search(array2, 72, 0, array2.length - 1, Integer::compareTo));
   }
+
 }
