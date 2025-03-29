d4b3fb434d2504042ff9625a77d04abf5d6f4f03
Khazhmurat Kairov <papayaspark@protonmail.com>
2025-03-29 12:15:18 +0300

    Remove unused imports from ThreeMemberClass

--- src/main/java/org/astonjava/p1/entities/ThreeMemberClass.java
+++ src/main/java/org/astonjava/p1/entities/ThreeMemberClass.java
@@ -2,6 +2,7 @@ package org.astonjava.p1.entities;
 
 // Общий абстрактный класс для трех классов. Может измениться по ходу проекта.
 
+import java.util.Comparator;
 import java.util.regex.Pattern;
 
 public abstract class ThreeMemberClass {
