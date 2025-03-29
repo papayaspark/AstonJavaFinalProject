d436cedb0615d468e88da3db3d6439adb8dbd86f
Khazhmurat Kairov <papayaspark@protonmail.com>
2025-03-29 12:06:55 +0300

    Add ComparingStrategy class (product of ComparingStrategyBuilder)

--- src/main/java/org/astonjava/p1/strategy/ComparingStrategy.java
+++ src/main/java/org/astonjava/p1/strategy/ComparingStrategy.java
@@ -4,9 +4,13 @@ import org.astonjava.p1.entities.ThreeMemberClass;
 
 import java.util.Comparator;
 
-public class ComparingStrategy<T> {
+public class ComparingStrategy<T> implements Comparator<T>{
 
     Comparator<T> comparator = null;
+
+    ComparingStrategy(Comparator<T> comparator) {
+        this.comparator = comparator;
+    }
 
     public int compare(T o1, T o2) {
         return comparator.compare(o1, o2);
