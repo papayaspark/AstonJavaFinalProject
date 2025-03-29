bcfd9ddee06a161eee436d9825100f5fe5630a17
Khazhmurat Kairov <papayaspark@protonmail.com>
2025-03-29 12:16:56 +0300

    Remove unused imports and main() from ComparingStrategyBuildr

--- src/main/java/org/astonjava/p1/strategy/ComparingStrategyBuilder.java
+++ src/main/java/org/astonjava/p1/strategy/ComparingStrategyBuilder.java
@@ -1,9 +1,7 @@
 package org.astonjava.p1.strategy;
 
-import org.astonjava.p1.entities.Bus;
 import org.astonjava.p1.entities.ThreeMemberClass;
 
-import java.util.Arrays;
 import java.util.Comparator;
 import java.util.LinkedList;
 
@@ -12,62 +10,17 @@ public class ComparingStrategyBuilder<T extends ThreeMemberClass> {
 
   LinkedList<Comparator<T>> comparators = new LinkedList<>();
 
-//  // TODO: Убрать, если другой окажется ОК
-//  public ComparingStrategyBuilder<T> addComparator(Comparator<T> comparator, boolean reverse) {
-//    if (reverse) comparators.add(comparator.reversed());
-//    else comparators.add(comparator);
-//    return this;
-//  }
-
   public ComparingStrategyBuilder<T> addComparator(Comparator<T> comparator) {
     comparators.add(comparator);
     return this;
   }
 
-  public Comparator<T> build() {
+  public ComparingStrategy<T> build() {
     if (comparators.isEmpty()) return null;
     Comparator<T> result = comparators.getFirst();
     for (int i = 1; i < comparators.size(); i++) {
       result = result.thenComparing(comparators.get(i));
     }
-    return result;
-  }
-
-  public static void main(String[] args) throws NoSuchMethodException, NoSuchFieldException, IllegalAccessException {
-    Bus[] bus = {
-      new Bus("100, KurganAuto, 2000"),
-      new Bus("2, Mercedes, 100"),
-      new Bus("1, SHL, 3434"),
-      new Bus("100, KurganAuto, 2001"),
-      new Bus("2, Mercedes, 100"),
-      new Bus("1, SHL, 3434"),
-      new Bus("100, KurganAuto, 2001"),
-      new Bus("100, Mercedes, 2001"),
-      new Bus("100, KurganAuto, 2002")
-    };
-//    System.out.println(Arrays.toString(Bus.class.getDeclaredMethods()));
-//    Class<?> workingClass = Bus.class;
-//    ComparingStrategyBuilder<Bus> builder = new ComparingStrategyBuilder<>();
-//    builder
-//        .addComparator((Comparator<Bus>) workingClass.getField("member1Comparator").get(null), false)
-//        .addComparator(Bus.member2Comparator, true)
-//        .addComparator(Bus.member3Comparator, false);
-//    Comparator<Bus> comparator = builder.build();
-
-    String[] order = new String[]{"1", "-2", "3"};
-    Comparator<Bus> comparator = Bus.createSortingStrategy(order);
-
-    Arrays.sort(bus, comparator);
-//    ComparingStrategyBuilder<? extends ThreeMemberClass> builder = new ComparingStrategyBuilder<>();
-//    Class<? extends ThreeMemberClass> workingClass = Bus.class;
-//    try {
-//      Comparator<? extends ThreeMemberClass> comp = (Comparator<ThreeMemberClass>) workingClass.getField("member1Comparator").get(null);
-//      System.out.println(workingClass.getField("member1Comparator").get(null));
-//      Arrays.sort(bus, comp);
-//    } catch (Exception e) {
-//      e.printStackTrace();
-//    }
-
-    for (var b : bus) System.out.println(b);
+    return new ComparingStrategy<T>(result);
   }
 }
